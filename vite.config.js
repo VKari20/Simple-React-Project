31ac4583a49bfd8ef76f76e64d7d92be78855add
Vennesa <njugunawangari82@gmail.com>
2025-07-17 22:34:14 +0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/week-3-react-js-assignment-VKari20/', 
+  base: '/week-3-react-js-assignment-VKari20/', // 👈 add this line
   plugins: [react()],
 })
